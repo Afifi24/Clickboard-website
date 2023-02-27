2d4588d711af57a480aa8751b0b70c8710f4d63c
afifi brahim <afifi71brahim@gmail.com>
2023-02-27 21:53:27 +0100

    fix the bug2

--- src/components/Workflow.jsx
+++ src/components/Workflow.jsx
@@ -15,21 +15,21 @@ const Workflow = () => {
         <h2 className='text-3xl font-bold my-3 text-DarkGrayishBlue'>Supercharge your workflow</h2>
         <p className='text-gray-500 md:max-w-sm'>We have got the tools to boost your productivity.</p>
       </div>
-      <div className='space-y-12 md:space-y-0 md:grid md:grid-cols-3 md:space-x-2 md:place-items-center md:mx-10 mb-10'>
+      <div className='space-y-12 md:space-y-0 md:grid md:grid-cols-3  md:place-items-center md:mx-28 md:items-center md:justify-center mb-10'>
       <div className='flex items-center flex-col '>  
         <img src={list} alt="" />
-        <h2 className='text-3xl font-bold my-3 text-DarkGrayishBlue'>Create blackists</h2>
-        <p className='text-gray-500 md:max-w-sm'>ensure sensitive information never makes its way to your clipboard by excluding certain sources.</p>
+        <h2 className='text-2xl font-bold my-3 text-DarkGrayishBlue'>Create blackists</h2>
+        <p className='text-gray-500 md:max-w-xs'>ensure sensitive information never makes its way to your clipboard by excluding certain sources.</p>
       </div>
-      <div className='flex items-center flex-col '>  
+      <div className='flex items-center flex-col md:mx-auto '>  
         <img src={text} alt="" />
-        <h2 className='text-3xl font-bold my-3 text-DarkGrayishBlue'>Plain text snippets</h2>
-        <p className='text-gray-500 md:max-w-sm'>remove unwanted formatting from copied text for a consistant look.</p>
+        <h2 className='text-2xl font-bold my-3 text-DarkGrayishBlue'>Plain text snippets</h2>
+        <p className='text-gray-500 md:max-w-xs'>remove unwanted formatting from copied text for a consistant look.</p>
       </div>
       <div className='flex items-center flex-col '>  
         <img src={preview} alt="" />
-        <h2 className='text-3xl font-bold my-3 text-DarkGrayishBlue'>sneak preview</h2>
-        <p className='text-gray-500 md:max-w-sm'>quick preview of all snippets on your clipboard for easy access.</p>
+        <h2 className='text-2xl font-bold my-3 text-DarkGrayishBlue'>sneak preview</h2>
+        <p className='text-gray-500 md:max-w-xs'>quick preview of all snippets on your clipboard for easy access.</p>
       </div>
       </div>
       <div className='flex flex-col justify-center text-center space-y-10 gap-0 md:flex-row md:space-y-0 md:space-x-4 mt-10 md:gap-6'>
