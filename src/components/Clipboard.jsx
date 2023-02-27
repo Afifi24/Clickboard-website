cc2fa787e1c6f9fb888251f1797a669413bbd795
afifi brahim <afifi71brahim@gmail.com>
2023-02-27 21:52:03 +0100

    fix the bug2

--- src/components/Clipboard.jsx
+++ src/components/Clipboard.jsx
@@ -6,7 +6,7 @@ const Clipboard = () => {
        <div className='mx-6 space-y-6'>
        <h2 className='font-bold my-4 text-3xl text-DarkGrayishBlue'>Clipbord for iOS and Mac OS</h2>
        <p className='md:max-w-[638px] text-gray-500'>Available for free on the App Store.Download for mac or iOS, sync with icloud and your are ready to start adding to your clipboard. </p>
-       <div className='flex flex-col space-y-6 md:flex-row md:space-y-0 md:space-x-4 justify-center'>
+       <div className='flex flex-col  space-y-6 md:flex-row md:space-y-0 md:space-x-4 justify-center'>
         <button className='py-4 px-6 bg-strongcyan hover:opacity-80 rounded-full font-bold text-white hover:-translate-y-0.5 hover:shadow-lg transition duration-200'>Download for iOS</button>
         <button className='py-4 px-6 bg-LightBlue rounded-full font-bold text-white hover:-translate-y-0.5 hover:shadow-lg transition duration-200 hover:opacity-80'>Download for Mac</button>
        </div>
